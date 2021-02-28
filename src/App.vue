6ad03025f168699052128bcde77cc1d9cd65dfcb
Ana Freitas <ana_paula.10@hotmail.com>
2021-02-28 01:36:56 -0300

    Add routes no reloading page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="corpo">
-    /*ora exibe um componente ora exibe outro*/
+    <nav>
+      <ul>       
+          <li v-for="router in routes" :key="router.path">
+              <router-link :to="router.path ? router.path : '/'"> {{ router.titulo}}</router-link>
+          </li>
+      </ul>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+
+import {routes} from './routes';
+
 export default{
-    
-  }
+  data(){
+    return {
+      routes
+    }
+  }    
+}
+
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import { routes } from './routes';
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'}) //ou routes: routes, como é o mesmo nome, podemos deixar implicito
 
 new Vue({
   el: '#app',
+  router, //ou router: router
   render: h => h(App)
 })
