517cc1a09e0805b188b7714bf32a1609e09ed7e9
Ana Freitas <ana_paula.10@hotmail.com>
2021-02-28 01:43:25 -0300

    transation between pages

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,10 @@
           </li>
       </ul>
     </nav>
-    <router-view></router-view>
+
+    <transition name="pagina-fade">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -30,5 +33,13 @@ export default{
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
+  }
+
+  .pagina-fade-enter, .pagina-fade-leave-active {
+      opacity: 0;
+  }
+
+  .pagina-fade-enter-active, .pagina-fade-leave-active {
+      transition: opacity .4s;
   }
 </style>
