dac6947925c51dc194936781f6444905f0372873
Ana Freitas <ana_paula.10@hotmail.com>
2021-02-28 15:50:59 -0300

    Pass parameters to directive

--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -1,15 +1,19 @@
 import Vue from 'vue';
 
 Vue.directive('meu-transform', {
-    //el -> referencia do DOM
+    //el -> referencia do DOM , binding-> pegar parametros
     bind(el, binding, vnode){
         let current = 0;
         el.addEventListener('dblclick', function(){
-            current+=90;
+            
+            let increment = 90;
+            let animate = false;
+            increment = binding.value.increment || increment;
+            animate = binding.value.animate || animate;
+            current+=increment;
+
+            if(animate) el.style.transition = 'transform 0.5s';
             el.style.transform = `rotate(${current}deg)`;
         })
-    
-
-        console.log(el);
     }
 }); 
